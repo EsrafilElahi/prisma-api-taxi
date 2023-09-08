9828ede1a992245621a4801924ee46e46550691e
esrafilelahi <essinho1121@gmail.com>
2023-09-08 20:00:28 +0330

    clean up controller

--- controller/driverController/createDriver.js
+++ controller/driverController/createDriver.js
@@ -1,1 +1,19 @@
-import { prisma } from '@prisma'
+import { prisma } from "@prisma";
+
+
+export default async function articleCreatePrisma() {
+  const article = await prisma.user.create({
+    data: {
+      ...info,
+      slug,
+      authorUsername,
+      tagList: { connect: tagList },
+    },
+    include: {
+      author: { include: { followedBy: true } },
+      tagList: true,
+      _count: { select: { favoritedBy: true } },
+    },
+  });
+  return article;
+}
