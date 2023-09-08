fcc0b87437c8670bb8fe9d5be60cb92ea30c4c6f
esrafilelahi <essinho1121@gmail.com>
2023-09-08 22:54:05 +0330

    clean up controller

--- controller/tripController/createTrip.js
+++ controller/tripController/createTrip.js
@@ -3,17 +3,7 @@ const prisma = require("./prisma");
 
 const createTrip = async () => {
   const article = await prisma.user.create({
-    data: {
-      ...info,
-      slug,
-      authorUsername,
-      tagList: { connect: tagList },
-    },
-    include: {
-      author: { include: { followedBy: true } },
-      tagList: true,
-      _count: { select: { favoritedBy: true } },
-    },
+    data: {}
   });
   return article;
 }
