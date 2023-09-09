ea31f3bd0fc75fdbc9deeaab269b0deef423d34d
esrafilelahi <essinho1121@gmail.com>
2023-09-09 13:51:51 +0330

    get drivers added

--- controller/driverController/getDriver.js
+++ controller/driverController/getDriver.js
@@ -1,20 +1,30 @@
 const prisma = require("../../prisma");
 
 
-const getDriver = async () => {
+const getDriver = async (req, res, next) => {
   const driver = await prisma.driver.findFirst({});
   return driver;
 }
 
-const getDrivers = async () => {
+const getDrivers = async (req, res, next) => {
   try {
-    const drivers = await prisma.driver.findMany({});
-  
+    const drivers = await prisma.driver.findMany({
+      select: {
+        id: true,
+        name: true,
+        email: true,
+        role: true,
+        profile: true,
+        trips: true,
+        vehicle: true,
+      }
+    });
+
     // driver exist
     if (!drivers) {
       return res.json({ message: "drivers not found", status: 404 })
     }
-  
+
     return res.json({ message: "drivers", status: 200, drivers: drivers })
   } catch (error) {
     return res.json({ message: "server error", status: 500, error: error })
