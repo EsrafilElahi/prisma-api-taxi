fb701474fb8c8fc252cf1dd9275403b7e4f31881
esrafilelahi <essinho1121@gmail.com>
2023-09-09 14:19:28 +0330

    add create passenger

--- controller/passenferController/createPassenger.js
+++ controller/passenferController/createPassenger.js
@@ -1,11 +1,31 @@
 const prisma = require("../../prisma");
 
 
-const createPassenger = async () => {
-  const passenger = await prisma.passenger.create({
-    data: {}
-  });
-  return passenger;
+const createPassenger = async (res, req, next) => {
+  try {
+    const passenger = await prisma.passenger.findUnique({
+      where: {
+        email: req.body.email
+      }
+    })
+
+    // passenger exists
+    if (passenger) {
+      return res.json({ message: "user already exists", status: 409 })
+    }
+
+    const newPassenger = await prisma.passenger.create({
+      data: {
+        name: req.body.name,
+        email: req.body.email
+      }
+    })
+
+    return res.json({ message: "user successfully created", status: 201, passenger: newPassenger })
+
+  } catch (error) {
+    return res.json({ message: "server error", status: 500, error: error })
+  }
 }
 
 module.exports = createPassenger
