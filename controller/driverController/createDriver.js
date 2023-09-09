eea77d5791ad38b8bf2261ef29dc52611c998a84
esrafilelahi <essinho1121@gmail.com>
2023-09-09 13:34:53 +0330

    add create driver method

--- controller/driverController/createDriver.js
+++ controller/driverController/createDriver.js
@@ -2,15 +2,38 @@ const prisma = require("../../prisma");
 
 
 const createDriver = async (req, res, next) => {
-  console.log(req);
-  
-  const driver = await prisma.driver.create({
-    data: {},
-  });
-  return driver;
+
+  try {
+    console.log(req.body);
+
+    // check user exist
+    const user = await prisma.driver.findUnique({
+      where: {
+        email: req.body.email
+      }
+    })
+
+    console.log('user exists :', user)
+
+    if (user) {
+      return res.json({ message: "user already exists", status: 409 })
+    }
+
+    const driver = await prisma.driver.create({
+      data: {
+        name: req.body.name,
+        email: req.body.email,
+        vehicle: req.body.vehicle,
+      },
+    });
+
+    return res.json({ message: "user created successfully", status: 201, data: driver })
+  } catch (error) {
+    return res.json({ error: 'server error', status: 500 })
+  }
 }
 
 module.exports = createDriver
 
 
-https://github.com/SeuRonao/realworld-express-prisma/blob/main/src/utils/db/article/articleListPrisma.ts
+// https://github.com/SeuRonao/realworld-express-prisma/blob/main/src/utils/db/article/articleListPrisma.ts
