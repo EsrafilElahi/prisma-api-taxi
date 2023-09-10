18b3b63a45fab397264c68ef9e34fa75444eb416
esrafilelahi <essinho1121@gmail.com>
2023-09-10 11:51:59 +0330

    update passenger route

--- routes/passenger.js
+++ routes/passenger.js
@@ -8,5 +8,6 @@ router.get("/", passengerController.getPassengers);
 router.get("/:passengerId", passengerController.getPassenger);
 router.post("/", passengerController.createPassenger);
 router.delete("/:passengerId", passengerController.deletePassenger);
+router.put("/:passengerId", passengerController.updatePassenger);
 
 module.exports = router;
