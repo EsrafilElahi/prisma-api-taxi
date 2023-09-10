4f57fe2a0de38ec43f27cb038bd692ead9c655c2
esrafilelahi <essinho1121@gmail.com>
2023-09-10 11:31:31 +0330

    update passenger controller

--- controller/passenferController/index.js
+++ controller/passenferController/index.js
@@ -1,5 +1,6 @@
 const createPassenger = require("./createPassenger");
 const { getPassenger } = require("./getPassenger");
 const { getPassengers } = require("./getPassenger");
+const deletePassenger = require("./deletePassenger");
 
-module.exports = { createPassenger, getPassenger, getPassengers }
+module.exports = { createPassenger, getPassenger, getPassengers, deletePassenger }
