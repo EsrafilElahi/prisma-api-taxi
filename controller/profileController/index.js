c28a78651ad3d0e425ae192e28e469fff884f9d7
esrafilelahi <essinho1121@gmail.com>
2023-09-10 12:38:31 +0330

    profile controller update

--- controller/profileController/index.js
+++ controller/profileController/index.js
@@ -1,4 +1,5 @@
 const createProfile = require("./createProfile");
 const { getPassengerProfile, getDriverProfile } = require("./getProfile");
+const updateProfile = require("./updateProfile");
 
-module.exports = { createProfile, getPassengerProfile, getDriverProfile }
+module.exports = { createProfile, getPassengerProfile, getDriverProfile, updateProfile }
