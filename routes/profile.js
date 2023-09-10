091bfcb314537106f798471b14d6199903a62ed8
esrafilelahi <essinho1121@gmail.com>
2023-09-10 12:42:48 +0330

    route prifile added

--- routes/profile.js
+++ routes/profile.js
@@ -7,5 +7,8 @@ const router = Router();
 router.get("/passenger/:passengerId", profileController.getPassengerProfile);
 router.get("/driver/:driverId", profileController.getDriverProfile);
 router.post("/", profileController.createProfile);
+router.get("/passenger/:passengerId", profileController.getPassengerProfile);
+router.get("/driver/:driverId", profileController.getDriverProfile);
+
 
 module.exports = router;
