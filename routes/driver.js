43272949800d731b2e6c47841fb3744b7e6b3013
esrafilelahi <essinho1121@gmail.com>
2023-09-10 11:31:53 +0330

    add route driver derlete

--- routes/driver.js
+++ routes/driver.js
@@ -7,5 +7,6 @@ const router = Router();
 router.get("/", driverController.getDrivers);
 router.get("/:driverId", driverController.getDriver);
 router.post("/", driverController.createDriver);
+router.delete("/:driverId", driverController.deleteDriver);
 
 module.exports = router;
