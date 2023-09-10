7b609af7b892f424f840e8ad9530e3cfaa5a5a13
esrafilelahi <essinho1121@gmail.com>
2023-09-10 11:50:58 +0330

    update driverController

--- controller/driverController/index.js
+++ controller/driverController/index.js
@@ -2,5 +2,6 @@ const createDriver = require("./createDriver");
 const { getDriver } = require("./getDriver");
 const { getDrivers } = require("./getDriver");
 const deleteDriver = require("./deleteDriver");
+const updateDriver = require("./updateDriver");
 
-module.exports = { createDriver, getDriver, getDrivers, deleteDriver }
+module.exports = { createDriver, getDriver, getDrivers, deleteDriver, updateDriver }
